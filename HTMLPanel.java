55bb247301bd63012bea92eb99a8061c31085ca7
Martin <martinszyd@poczta.fm>
2018-07-03 09:36:13 +0200

    Little changes in converting method

--- HTMLPanel.java
+++ HTMLPanel.java
@@ -2,8 +2,7 @@ package TextFields;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class HTMLPanel extends JPanel {
 
@@ -24,20 +23,21 @@ public class HTMLPanel extends JPanel {
         //ustawiamy nasz editorPane, aby rozpoznawa znaczniki html
         editorPane.setContentType("text/html");
         //przycisk generowania podglądu
-        JButton actionButton = new JButton("View");
-        actionButton.addActionListener(new ConvertListener());
+        //  JButton actionButton = new JButton("View");
+        //  actionButton.addActionListener(new ConvertListener());
         //panel pomocniczy do rozkładu elementów
         JPanel helpPanel = new JPanel();
         helpPanel.setLayout(new GridLayout(1, 2));
         textArea.setBackground(Color.lightGray);
         //dodajemy komponenty tekstowe do pomocniczego panelu
         helpPanel.add(textArea);
+        textArea.addKeyListener(new MyKeyListener());
         helpPanel.add(editorPane);
         //dodajemy wszystko do głównego panelu
         this.add(helpPanel, BorderLayout.CENTER);
-        this.add(actionButton, BorderLayout.SOUTH);
+        //  this.add(actionButton, BorderLayout.SOUTH);
     }
-
+/*
     class ConvertListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent event) {
@@ -48,6 +48,19 @@ public class HTMLPanel extends JPanel {
                     String text = textArea.getText();
                     editorPane.setText(text);
                     editorPane.revalidate();
+                }
+            });*/
+
+    class MyKeyListener extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent arg0) {
+            //zmiany wyglądu wywołujemy w wątku dystrybucji zdarzeń
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    String text = textArea.getText();
+                    editorPane.setText(text);
                 }
             });
         }
